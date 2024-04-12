2a4c70b3fbf311053a3b47bdda06532b4a8e0459
Bruno Gois Costa <53541903+brunog-costa@users.noreply.github.com>
2024-04-12 11:15:24 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -15,29 +15,7 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 disable_warnings(packages.urllib3.exceptions.InsecureRequestWarning)
 
-# Log HTTP connection
-# http_client.HTTPConnection.debuglevel = 1
-# req_log = logging.getLogger('requests.packages.urllib3')
-# req_log.setLevel(logging.DEBUG)
-# req_log.propagate = True
-
-
-def lambda_handler(event, context):
-    """Execute the main code.
-
-    Parameters
-    ----------
-    event : string
-        Contains a JSON with the message and the metadata.
-    context : string
-        Contains a JSON with the context of the execution.
-
-    Returns
-    -------
-    dict
-        A JSON with a friendly message.
-    """
-    def send_data_to_splunk(payload: str,
+def send_data_to_splunk(payload: str,
                             splunk_token: str,
                             splunk_url: str) -> dict:
         """Send the data to the Splunk cluster.
@@ -65,54 +43,58 @@ def lambda_handler(event, context):
         except Exception:
             logging.error(f'Error: {print_traceback()}')
             return {"statusCode": 400}
+
+def lambda_handler(event, context):
+    """Execute the main code.
+
+    Parameters
+    ----------
+    event : string
+        Contains a JSON with the message and the metadata.
+    context : string
+        Contains a JSON with the context of the execution.
+
+    Returns
+    -------
+    dict
+        A JSON with a friendly message.
+    """
+    
     # The Splunk URL
-    splunk_url = environ['SPLUNK_URL']
+    splunk_url = environ['SPLUNK_HEC_URL']
     # The AWS SecretsManager Client object
     secrets_client = client_boto3('secretsmanager')
     # The Splunk Token
     splunk_token = secrets_client.get_secret_value(
-        SecretId=environ['SPLUNK_TOKEN_SECRET'])['SecretString']
+        SecretId=environ['SPLUNK_HEC_TOKEN'])['SecretString']
 
     # The AWS S3 Client object
     s3_client = client_boto3('s3')
     # Get the objects in the bucket
     for record in event['Records']:
         bucket = loads(record['body'])['Records'][0]['s3']['bucket']['name']
-        key = unquote_plus(loads(record['body'])
-                           ['Records'][0]['s3']['object']['key'])
-        # Exclude the files from the hash validation folders
-        if ('CloudTrail-Digest' not in key and
-                'Config' not in key):
-            # print(record)
-            # Download the  from the S3
-            response = s3_client.get_object(Bucket=bucket, Key=key)
-            # Read the object
-            content = response['Body'].read()
-            # GUnzip the downloaded object
-            with GzipFile(fileobj=BytesIO(content), mode='rb') as fh:
-                # Check if 'fh' is a empty binary file
-                if not fh:
-                    log.info("Skipping, empty binary file found after" +
-                             " unzipping GZIP. Going to the next...")
-                    break
-                # Try to load the JSON content
-                try:
-                    first_json = load(fh)
-                except Exception:
-                    log.error(f"Error while loading the JSON: {fh.readline()}")
-                    log.error(f"Failed while reading: File={key}")
-                    break
-                # Group the events to reduce network requests
-                if 'Records' in first_json:
-                    result = list()
-                    for log_event in first_json['Records']:
-                        result.append({"index": "aws_cloudtrail",
-                                       "sourcetype": "aws:cloudtrail",
-                                       "source": "http:AWS_CLOUDTRAIL_US_EAST_1",
-                                       "event": dumps(log_event)})
-                    # print(result)
-                    send_data_to_splunk(result,
-                                        splunk_token,
-                                        splunk_url)
+        key = unquote_plus(loads(record['body'])['Records'][0]['s3']['object']['key'])
+        # Download the  from the S3
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        # Read the object
+        content = response['Body'].read()
+        # GUnzip the downloaded object
+        with GzipFile(fileobj=BytesIO(content), mode='rb') as fh:
+            # Try to load the JSON content and group the events to reduce network requests
+            try:
+                result = []
+                for line in fh:
+                    # Here we change the encoding for keeping special characters 
+                    line = loads(line)
+                    line = loads(dumps(line).encode('latin1').decode('unicode_escape'))
+                    result.append({"index": "aws_others",
+                                "sourcetype": "datamesh:aws_account",
+                                "event": line})
+                postrep = send_data_to_splunk(result, splunk_token, splunk_url)
+            except Exception:
+                log.error(f"Failed while reading: File={key}")
+                log.error(print_traceback())
+                return {"statusCode": 500}
+        log.info(postrep)
     # Return success to the Lambda execution
     return {"statusCode": 200}
