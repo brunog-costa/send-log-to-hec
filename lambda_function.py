c4fbf04866349718a7b6c16d4d7b39ed403cde88
Bruno Gois Costa <53541903+brunog-costa@users.noreply.github.com>
2024-04-12 11:05:58 -0300

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,118 @@
+import http.client as http_client
+import logging
+from gzip import GzipFile
+from io import BytesIO
+from json import dumps, load, loads
+from os import environ
+from traceback import format_exc as print_traceback
+from urllib.parse import unquote_plus
+from boto3 import client as client_boto3
+from requests import packages, post
+from urllib3 import disable_warnings
+
+# Local logging setup
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+disable_warnings(packages.urllib3.exceptions.InsecureRequestWarning)
+
+# Log HTTP connection
+# http_client.HTTPConnection.debuglevel = 1
+# req_log = logging.getLogger('requests.packages.urllib3')
+# req_log.setLevel(logging.DEBUG)
+# req_log.propagate = True
+
+
+def lambda_handler(event, context):
+    """Execute the main code.
+
+    Parameters
+    ----------
+    event : string
+        Contains a JSON with the message and the metadata.
+    context : string
+        Contains a JSON with the context of the execution.
+
+    Returns
+    -------
+    dict
+        A JSON with a friendly message.
+    """
+    def send_data_to_splunk(payload: str,
+                            splunk_token: str,
+                            splunk_url: str) -> dict:
+        """Send the data to the Splunk cluster.
+
+        Parameters
+        ----------
+        payload : str
+            A string with a JSON object that will be sent.
+        splunk_token : str
+            The token used to authenticate the communication.
+        splunk_url : str
+            The URL with the destination of the package.
+
+        Returns
+        -------
+        None
+        """
+        header = {'Authorization': 'Splunk ' + splunk_token}
+        try:
+            return post(splunk_url,
+                        headers=header,
+                        json=payload,
+                        verify=0,
+                        timeout=90).json()
+        except Exception:
+            logging.error(f'Error: {print_traceback()}')
+            return {"statusCode": 400}
+    # The Splunk URL
+    splunk_url = environ['SPLUNK_URL']
+    # The AWS SecretsManager Client object
+    secrets_client = client_boto3('secretsmanager')
+    # The Splunk Token
+    splunk_token = secrets_client.get_secret_value(
+        SecretId=environ['SPLUNK_TOKEN_SECRET'])['SecretString']
+
+    # The AWS S3 Client object
+    s3_client = client_boto3('s3')
+    # Get the objects in the bucket
+    for record in event['Records']:
+        bucket = loads(record['body'])['Records'][0]['s3']['bucket']['name']
+        key = unquote_plus(loads(record['body'])
+                           ['Records'][0]['s3']['object']['key'])
+        # Exclude the files from the hash validation folders
+        if ('CloudTrail-Digest' not in key and
+                'Config' not in key):
+            # print(record)
+            # Download the  from the S3
+            response = s3_client.get_object(Bucket=bucket, Key=key)
+            # Read the object
+            content = response['Body'].read()
+            # GUnzip the downloaded object
+            with GzipFile(fileobj=BytesIO(content), mode='rb') as fh:
+                # Check if 'fh' is a empty binary file
+                if not fh:
+                    log.info("Skipping, empty binary file found after" +
+                             " unzipping GZIP. Going to the next...")
+                    break
+                # Try to load the JSON content
+                try:
+                    first_json = load(fh)
+                except Exception:
+                    log.error(f"Error while loading the JSON: {fh.readline()}")
+                    log.error(f"Failed while reading: File={key}")
+                    break
+                # Group the events to reduce network requests
+                if 'Records' in first_json:
+                    result = list()
+                    for log_event in first_json['Records']:
+                        result.append({"index": "aws_cloudtrail",
+                                       "sourcetype": "aws:cloudtrail",
+                                       "source": "http:AWS_CLOUDTRAIL_US_EAST_1",
+                                       "event": dumps(log_event)})
+                    # print(result)
+                    send_data_to_splunk(result,
+                                        splunk_token,
+                                        splunk_url)
+    # Return success to the Lambda execution
+    return {"statusCode": 200}
